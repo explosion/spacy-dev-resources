44ffc15dfe699ff0b7984f6f21cd85b5dd61db54
Matthew Honnibal <honnibal+gh@gmail.com>
2016-12-18 21:44:56 +0100

    Update init.py script for reorganized language_data

--- training/init.py
+++ training/init.py
@@ -1,20 +1,4 @@
-"""Set up a model directory.
-
-Requires:
-
-    lang_data --- Rules for the tokenizer
-        * prefix.txt
-        * suffix.txt
-        * infix.txt
-        * morphs.json
-        * specials.json
-
-    corpora --- Data files
-        * WordNet
-        * words.sgt.prob --- Smoothed unigram probabilities
-        * clusters.txt --- Output of hierarchical clustering, e.g. Brown clusters
-        * vectors.bz2 --- output of something like word2vec, compressed with bzip
-"""
+"""Set up a model directory."""
 from __future__ import unicode_literals
 
 from ast import literal_eval
@@ -45,17 +29,6 @@ except NameError:
     unicode = str
 
 
-def setup_tokenizer(lang_data_dir, tok_dir):
-    if not tok_dir.exists():
-        tok_dir.mkdir()
-
-    for filename in ('infix.txt', 'morphs.json', 'prefix.txt', 'specials.json',
-                     'suffix.txt'):
-        src = lang_data_dir / filename
-        dst = tok_dir / filename
-        copyfile(str(src), str(dst))
-
-
 def _read_clusters(loc):
     if not loc.exists():
         print("Warning: Clusters file not found")
@@ -83,19 +56,7 @@ def _read_clusters(loc):
     return clusters
 
 
-def _read_probs(loc):
-    if not loc.exists():
-        print("Probabilities file not found. Trying freqs.")
-        return {}, 0.0
-    probs = {}
-    for i, line in enumerate(io.open(str(loc), 'r', encoding='utf8')):
-        prob, word = line.split()
-        prob = float(prob)
-        probs[word] = prob
-    return probs, probs['-OOV-']
-
-
-def _read_freqs(loc, max_length=100, min_doc_freq=5, min_freq=200):
+def _read_probs_from_freqs(loc, max_length=100, min_doc_freq=5, min_freq=200):
     if not loc.exists():
         print("Warning: Frequencies file not found")
         return {}, 0.0
@@ -129,52 +90,16 @@ def _read_freqs(loc, max_length=100, min_doc_freq=5, min_freq=200):
     return probs, oov_prob
 
 
-def _read_senses(loc):
-    lexicon = defaultdict(lambda: defaultdict(list))
-    if not loc.exists():
-        print("Warning: WordNet senses not found")
-        return lexicon
-    sense_names = dict((s, i) for i, s in enumerate(spacy.senses.STRINGS))
-    pos_ids = {'noun': NOUN, 'verb': VERB, 'adjective': ADJ}
-    for line in codecs.open(str(loc), 'r', 'utf8'):
-        sense_strings = line.split()
-        word = sense_strings.pop(0)
-        for sense in sense_strings:
-            pos, sense = sense[3:].split('.')
-            sense_name = '%s_%s' % (pos[0].upper(), sense.lower())
-            if sense_name != 'N_tops':
-                sense_id = sense_names[sense_name]
-                lexicon[word][pos_ids[pos]].append(sense_id)
-    return lexicon
-
-
-def setup_vocab(lex_attr_getters, tag_map, src_dir, dst_dir):
-    if not dst_dir.exists():
-        dst_dir.mkdir()
-    print('Reading vocab from ', src_dir)
-    vectors_src = src_dir / 'vectors.bz2'
-    if vectors_src.exists():
-        write_binary_vectors(vectors_src.as_posix(), (dst_dir / 'vec.bin').as_posix())
-    else:
-        print("Warning: Word vectors file not found")
-    vocab = Vocab(lex_attr_getters=lex_attr_getters, tag_map=tag_map)
-    clusters = _read_clusters(src_dir / 'clusters.txt')
-    probs, oov_prob = _read_probs(src_dir / 'words.sgt.prob')
-    if not probs:
-        probs, oov_prob = _read_freqs(src_dir / 'freqs.txt.gz')
-    if not probs:
-        oov_prob = -20
-    else:
-        oov_prob = min(probs.values())
+def populate_vocab(vocab, clusters, probs, oov_prob):
+    # Ensure probs has entries for all words seen during clustering.
     for word in clusters:
         if word not in probs:
             probs[word] = oov_prob
-
-    lexicon = []
-    for word, prob in reversed(sorted(list(probs.items()), key=lambda item: item[1])):
-        # First encode the strings into the StringStore. This way, we can map
-        # the orth IDs to frequency ranks
-        orth = vocab.strings[word]
+    #lexicon = []
+    #for word, prob in reversed(sorted(list(probs.items()), key=lambda item: item[1])):
+    #    # First encode the strings into the StringStore. This way, we can map
+    #    # the orth IDs to frequency ranks
+    #    orth = vocab.strings[word]
     # Now actually load the vocab
     for word, prob in reversed(sorted(list(probs.items()), key=lambda item: item[1])):
         lexeme = vocab[word]
@@ -186,43 +111,45 @@ def setup_vocab(lex_attr_getters, tag_map, src_dir, dst_dir):
             lexeme.cluster = int(clusters[word][::-1], 2)
         else:
             lexeme.cluster = 0
-    vocab.dump((dst_dir / 'lexemes.bin').as_posix())
-    with (dst_dir / 'strings.json').open('w') as file_:
-        vocab.strings.dump(file_)
-    with (dst_dir / 'oov_prob').open('w') as file_:
-        file_.write('%f' % oov_prob)
 
 
-def main(lang_id, lang_data_dir, corpora_dir, model_dir):
+def write_vectors(src_dir, dst_dir):
+    print('Reading vocab from ', src_dir)
+    vectors_src = src_dir / 'vectors.bz2'
+    if vectors_src.exists():
+        write_binary_vectors(vectors_src.as_posix(), (dst_dir / 'vec.bin').as_posix())
+    else:
+        print("Warning: Word vectors file not found")
+
+
+def main(lang_id, model_dir, freqs_loc, clusters_loc=None, vectors_loc=None):
     model_dir = Path(model_dir)
-    lang_data_dir = Path(lang_data_dir) / lang_id
-    corpora_dir = Path(corpora_dir) / lang_id
-
-    assert corpora_dir.exists()
-    assert lang_data_dir.exists()
+    freqs_loc = Path(freqs_loc)
+    if clusters_loc is not None:
+        clusters_loc = Path(clusters_loc)
+    if vectors_loc is not None:
+        vectors_loc = Path(vectors_loc)
 
     if not model_dir.exists():
         model_dir.mkdir()
 
-    tag_map = json.load((lang_data_dir / 'tag_map.json').open())
-    setup_tokenizer(lang_data_dir, model_dir / 'tokenizer')
-    setup_vocab(get_lang_class(lang_id).Defaults.lex_attr_getters, tag_map, corpora_dir,
-                model_dir / 'vocab')
+    vocab = get_lang_class(lang_id).Defaults.create_vocab()
 
-    if (lang_data_dir / 'gazetteer.json').exists():
-        copyfile((lang_data_dir / 'gazetteer.json').as_posix(),
-                 (model_dir / 'vocab' / 'gazetteer.json').as_posix())
+    clusters = _read_clusters(clusters_loc)
+    probs, oov_prob = _read_probs_from_freqs(freqs_loc)
+    populate_vocab(vocab, clusters, probs, oov_prob)
 
-    copyfile((lang_data_dir / 'tag_map.json').as_posix(),
-             (model_dir / 'vocab' / 'tag_map.json').as_posix())
-
-    if (lang_data_dir / 'lemma_rules.json').exists():
-        copyfile((lang_data_dir / 'lemma_rules.json').as_posix(),
-                 (model_dir / 'vocab' / 'lemma_rules.json').as_posix())
-
-    if not (model_dir / 'wordnet').exists() and (corpora_dir / 'wordnet').exists():
-        copytree((corpora_dir / 'wordnet' / 'dict').as_posix(),
-                 (model_dir / 'wordnet').as_posix())
+    if not model_dir.exists():
+        model_dir.mkdir()
+    if not (model_dir / 'vocab').exists():
+        (model_dir / 'vocab').mkdir()
+    vocab.dump((model_dir / 'vocab' / 'lexemes.bin').as_posix())
+    with (model_dir / 'vocab' / 'strings.json').open('w') as file_:
+        vocab.strings.dump(file_)
+    with (model_dir / 'vocab' / 'oov_prob').open('w') as file_:
+        file_.write('%f' % oov_prob)
+    if vectors_loc is not None:
+        write_vectors(vectors_loc, model_dir)
 
 
 if __name__ == '__main__':
