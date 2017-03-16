473f60d29f89246cf9eb27ffdc1029e71db385cd
ines <ines@ines.io>
2017-03-16 23:31:56 +0100

    Add model package template

---
+++ templates/model/en_model_name-v0.0.0/__init__.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from spacy.util import get_lang_class
+import pkg_resources
+from . import about
+
+
+def load_meta():
+    with io.open('meta.json', encoding='utf8') as f:
+        return json.load(f)
+
+
+def load(**kwargs):
+    version = load_meta()['version']
+    data_dir = pkg_resources.resource_filename(__name__, __name__ + '-' + version)
+    lang = get_lang_class(about.__lang__)
+    return lang(path=Path(data_dir), **kwargs)
---
+++ templates/model/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf8
+from __future__ import unicode_literals
+
+import io
+import json
+from os import path, walk
+from shutil import copy
+from setuptools import setup
+
+
+def load_meta(fp):
+    with io.open(fp, encoding='utf8') as f:
+        return json.load(f)
+
+
+def generate_about(fp, data):
+    with io.open(fp, 'w', encoding='utf8') as f:
+        for key in data.keys():
+            line = "__{k}__ = '{v}'\n".format(k=key, v=data[key])
+            f.write(line)
+
+
+def list_files(data_dir):
+    paths = []
+    for root, _, filenames in walk(data_dir):
+        for filename in filenames:
+            if not filename.startswith('.'):
+                paths.append(path.join(root, filename))
+    output = [p.split('/', 1)[1] for p in paths]
+    output.append('meta.json')
+    return output
+
+
+def setup_package():
+    root = path.abspath(path.dirname(__file__))
+    meta_path = path.join(root, 'meta.json')
+    meta = load_meta(meta_path)
+    model_name = str(meta['lang'] + '_' + meta['name'])
+    model_dir = path.join(model_name, model_name + '-' + meta['version'])
+
+    copy(meta_path, path.join(model_name))
+    copy(meta_path, model_dir)
+
+    setup(
+        name=model_name,
+        description=meta['description'],
+        author=meta['author'],
+        author_email=meta['email'],
+        url=meta['url'],
+        version=meta['version'],
+        license=meta['license'],
+        packages=[model_name],
+        package_data={model_name: list_files(model_dir)},
+        install_requires=['spacy' + meta['spacy_version']],
+        zip_safe=False,
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
