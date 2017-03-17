b1effcab4db254c4d89a54370456c4e8f4fd8787
ines <ines@ines.io>
2017-03-17 01:26:55 +0100

    Fix typos

--- templates/model/en_model_name-v0.0.0/__init__.py
+++ templates/model/en_model_name-v0.0.0/__init__.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 from spacy.util import get_lang_class
 import pkg_resources
+import json
 
 
 def load_meta():
-    with io.open('meta.json', encoding='utf8') as f:
+    with (Path(__file__).parent / 'meta.json').open() as f:
         return json.load(f)
 
 
 def load(**kwargs):
-    version = load_meta()['version']
+    version = load_meta()
+    version = meta['version']
     data_dir = pkg_resources.resource_filename(__name__, __name__ + '-' + version)
-    lang = get_lang_class(about.__lang__)
+    lang = get_lang_class(meta['lang'])
     return lang(path=Path(data_dir), **kwargs)
