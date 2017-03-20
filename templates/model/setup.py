68be1b1c4cdf796754ef6e981dece09e7ac11d9e
ines <ines@ines.io>
2017-03-20 19:18:06 +0100

    Update setup.py and remove unused code

--- templates/model/setup.py
+++ templates/model/setup.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 import io
 import json
 from os import path, walk
-from os.path import dirname
 from shutil import copy
 from setuptools import setup
 
@@ -15,20 +14,13 @@ def load_meta(fp):
         return json.load(f)
 
 
-def generate_about(fp, data):
-    with io.open(fp, 'w', encoding='utf8') as f:
-        for key in data.keys():
-            line = "__{k}__ = '{v}'\n".format(k=key, v=data[key])
-            f.write(line)
-
-
 def list_files(data_dir):
     output = []
     for root, _, filenames in walk(data_dir):
         for filename in filenames:
             if not filename.startswith('.'):
                 output.append(path.join(root, filename))
-    output = [path.relpath(p, dirname(data_dir)) for p in output]
+    output = [path.relpath(p, path.dirname(data_dir)) for p in output]
     output.append('meta.json')
     return output
 
