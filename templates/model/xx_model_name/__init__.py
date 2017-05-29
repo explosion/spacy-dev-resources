95f565aea57ced72b527695eefa9eb4512702def
ines <ines@ines.io>
2017-05-29 13:58:34 +0200

    Update new __init__.py template

--- templates/model/xx_model_name/__init__.py
+++ templates/model/xx_model_name/__init__.py
@@ -1,17 +1,8 @@
-from pathlib import Path
-from spacy.util import get_lang_class
-import pkg_resources
-import json
+# coding: utf8
+from __future__ import unicode_literals
+
+from spacy.util import load_model_from_init_py
 
 
-def load_meta():
-    with (Path(__file__).parent / 'meta.json').open() as f:
-        return json.load(f)
-
-
-def load(**kwargs):
-    meta = load_meta()
-    version = meta['version']
-    data_dir = pkg_resources.resource_filename(__name__, __name__ + '-' + version)
-    lang = get_lang_class(meta['lang'])
-    return lang(path=Path(data_dir), **kwargs)
+def load(**overrides):
+    return load_model_from_init_py(__file__, **overrides)
