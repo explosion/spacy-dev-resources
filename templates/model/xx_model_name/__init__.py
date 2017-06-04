d7fce4c20566e9cfe50f9d192aae9f15c34e3533
ines <ines@ines.io>
2017-06-04 21:28:08 +0200

    Make sure model exposes __version__

--- templates/model/xx_model_name/__init__.py
+++ templates/model/xx_model_name/__init__.py
@@ -1,7 +1,10 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from spacy.util import load_model_from_init_py
+from spacy.util import load_model_from_init_py, get_model_meta
+
+
+__version__ = get_model_meta(__file__)['version']
 
 
 def load(**overrides):
