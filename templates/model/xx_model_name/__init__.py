6d5c9109d2bf69f86fa1c4698a711fef9c52a34f
ines <ines@ines.io>
2017-06-04 21:35:40 +0200

    Fix version in model __init__.py

--- templates/model/xx_model_name/__init__.py
+++ templates/model/xx_model_name/__init__.py
@@ -1,10 +1,11 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+from pathlib import Path
 from spacy.util import load_model_from_init_py, get_model_meta
 
 
-__version__ = get_model_meta(__file__)['version']
+__version__ = get_model_meta(Path(__file__).parent)['version']
 
 
 def load(**overrides):
